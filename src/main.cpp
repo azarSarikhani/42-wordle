ee596df547274dcda28dbcacd30e80c0ed2e7bf2
Heikki Seppanen <6179953+heikkiseppanen@users.noreply.github.com>
2023-08-02 10:39:04 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+
+// app loop
+// 	main menu
+// 	load dictionary
+// 	pick word of the day
+// 	validate word of the day
+// 	loop max 6 times:
+// 		display guesses
+// 		prompt input
+// 		validate input
+// 		colorize input
+// 		if all right
+// 			quit
+
+int main()
+{
+
+}
