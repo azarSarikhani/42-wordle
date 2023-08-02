da0cf176f89210ddb09776cea213c41adb443c83
Heikki Seppanen <6179953+heikkiseppanen@users.noreply.github.com>
2023-08-02 15:53:02 +0300

    Added color printing of guesses

--- src/game.cpp
+++ src/game.cpp
@@ -3,9 +3,27 @@
 #include <cctype>
 #include<iostream>
 
+#define GREEN  "\e[0;32m"
+#define YELLOW "\e[0;33m"
+#define WHITE   "\e[0m"
+
+std::string	get_letter_color(char letter, std::size_t index, std::string& sample)
+{
+	for (std::size_t i = 0; i < sample.size(); ++i) {
+		if (letter == sample[i])
+		{
+			sample[i] = 0;
+			if (i == index)
+				return std::string(GREEN);
+			else
+				return std::string(YELLOW);
+		}
+	}
+	return std::string(WHITE);
+}
+
 void	game_on(std::string word)
 {
-	
 	Guesses guess;
 
 	for(int i = 0; i < 6; i++)
@@ -30,20 +48,28 @@ void	game_on(std::string word)
 			}
 
 			else
+			{
+				std::string new_line;
+				std::string copy = word;
+
+				for (std::size_t c = 0; c < guess.buffer.size(); ++c) 
+				{
+					std::string color = get_letter_color(guess.buffer[c], c, copy);
+					new_line += color + guess.buffer[c] + ' ';
+				}
+				new_line += WHITE;
+				guess.guess[i] = new_line;
+
 				if (guess.buffer == word)
 				{
+					for (const auto& member: guess.guess)
+					{
+						std::cout << member << std::endl;
+					}
 					printf("Yay you win\n");
 					return ;
 				}
-				else
-				{
-					int index = 0;
-					for (auto letter_buf : guess.buffer) 
-					{
-						guess.guess[i][index++] = letter_buf;
-						guess.guess[i][index++] = ' ';
-					}
-				} 
+			} 
 		}
 
 	}
